3221844ac1815caf9b0330828130bbc9c2136cdd
Sandile Keswa <sandile.keswa@gmail.com>
2013-11-07 00:37:56 -0500

    VERY crude session panel added

--- public/js/controllers/header.js
+++ public/js/controllers/header.js
@@ -1,6 +1,14 @@
 angular.module('mean.system').controller('HeaderController', ['$scope', 'Global', function ($scope, Global) {
     $scope.global = Global;
 
+    $scope.doRegister = function() {
+        alert("reg");
+    };
+
+    $scope.doSignIn = function() {
+        alert("sign");
+    };
+
     $scope.menu = [{
         "title": "Articles",
         "link": "articles"
