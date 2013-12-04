e54338c4d5527c57019db6ce574194d00899bb57
Sri Ramanujam <sricharan.ramanujam@gmail.com>
2013-12-03 19:49:30 -0500

    Misc fixes

--- acmwebsite/banner/models.py
+++ acmwebsite/banner/models.py
@@ -7,7 +7,7 @@ class Banner(models.Model):
     image = models.ImageField(upload_to="banner_pics", blank=False)
     created = models.DateTimeField(u'Date Created', blank=False, null=False,
                                    default=timezone.now())
-    name = models.CharField(max_length=1000)
+    name = models.CharField(max_length=200)
     blurb = models.TextField(max_length=500)
 
     ordering = ['-created']
