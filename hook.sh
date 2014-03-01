fe33cb52a9e5dd69173aaba07664368faf80e87d
Sandile Keswa <sandile.keswa@gmail.com>
2014-03-01 04:05:23 -0500

    updated the script for the git deploy

--- hook.sh
+++ hook.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-mkdir -p ~/deploy && cd ~/deploy && wget projectmaster.zip -q https://github.com/temple-acm/site/archive.zip
+mkdir -p ~/deploy && cd ~/deploy && wget -O projectmaster.zip -q https://github.com/temple-acm/site/archive/master.zip
 
 if [ -f ~/deploy/projectmaster.zip ]; then
 	unzip -q ~/deploy/projectmaster.zip
 	rm ~/deploy/projectmaster.zip
-	mv Project-master tuacm.org
+	mv site-master tuacm.org
 	rm -rf /srv/tuacm.org
 	mv tuacm.org /srv
 fi
