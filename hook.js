4f5c3c199217aef4baac1814ec4b12f5a53a49fa
Sandile Keswa <sandile.keswa@gmail.com>
2014-03-01 00:39:27 -0500

    updated hook to be more verbose

--- hook.js
+++ hook.js
@@ -3,6 +3,33 @@ var gith = require("gith").create(9000);
 // Import execFile, to run our bash script
 var execFile = require("child_process").execFile;
 
+function getDateTime() {
+	var now = new Date();
+	var year = now.getFullYear();
+	var month = now.getMonth() + 1;
+	var day = now.getDate();
+	var hour = now.getHours();
+	var minute = now.getMinutes();
+	var second = now.getSeconds();
+	if (month.toString().length == 1) {
+		var month = '0' + month;
+	}
+	if (day.toString().length == 1) {
+		var day = '0' + day;
+	}
+	if (hour.toString().length == 1) {
+		var hour = '0' + hour;
+	}
+	if (minute.toString().length == 1) {
+		var minute = '0' + minute;
+	}
+	if (second.toString().length == 1) {
+		var second = '0' + second;
+	}
+	var dateTime = year + '/' + month + '/' + day + ' ' + hour + ':' + minute + ':' + second;
+	return dateTime;
+}
+
 gith({
 	repo: "temple-acm/site"
 }).on("all", function (payload) {
@@ -10,7 +37,9 @@ gith({
 		// Exec a shell script
 		execFile("hook.sh", function (error, stdout, stderr) {
 			// Log success in some manner
-			console.log("Hook execution complete.");
+			console.log(getDateTime() + "\tExecuting git deploy of acm-site...\n");
+			console.log(stdout.toString("utf8"));
+			console.log(getDateTime() + "\tDONE\n.");
 		});
 	}
 });
