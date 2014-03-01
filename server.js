cfbf71a00f06c65907f80794384b3697fdc715d8
Sandile Keswa <sandile.keswa@gmail.com>
2014-03-01 02:13:02 -0500

    ..

--- server.js
+++ server.js
@@ -68,6 +68,7 @@ logger.info("HTTP route bootstrapping complete.");
 // Start the app by listening on <port>
 var port = process.env.PORT || config.port;
 var securePort = (parseInt(port) + 1);
+
 // Create an HTTP service.
 console.log(); // Newline for cleanliness
 http.createServer(app).listen(port, "127.0.0.1");
