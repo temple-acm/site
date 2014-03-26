40d6782f5ebf6ba992f94bb336a5d8de3c8d7ede
Sandile Keswa <me@sandile.io>
2014-03-26 19:23:09 -0400

    added a button for gitlab

--- public/js/controllers/sidebar.js
+++ public/js/controllers/sidebar.js
@@ -1,6 +1,10 @@
-angular.module("mean.system").controller("SidebarController", function ($scope, $location) {
+angular.module("mean.system").controller("SidebarController", function ($scope, $location, $window) {
 	$scope.navClass = function (page) {
 		var currentRoute = $location.path().substring(1) || "home";
 		return page === currentRoute ? "current" : "";
+	};
+
+	$scope.gotoGitlab = function() {
+		$window.location.href = '/gitlab';
 	};
 });
