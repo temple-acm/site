48ec6b1f026956a73328a5f75dac47c4f73b612b
Sandile Keswa <sandile.keswa@gmail.com>
2014-03-27 19:50:15 -0400

    changes to how we do the registration

--- backend/models/user.js
+++ backend/models/user.js
@@ -12,8 +12,12 @@ var mongoose = require('mongoose'),
  * User Schema
  */
 var UserSchema = new Schema({
-    name: String,
+    firstName: String,
+    lastName: String,
     email: String,
+    bio: String,
+    major: String,
+    studentLevel: String,
     username: {
         type: String,
         unique: true
@@ -21,60 +25,31 @@ var UserSchema = new Schema({
     provider: String,
     hashed_password: String,
     salt: String,
-    facebook: {},
-    twitter: {},
-    github: {},
-    google: {}
+    facebook: String,
+    twitter: String,
+    github: String
 });
 
 /**
  * Virtuals
  */
-UserSchema.virtual('password').set(function(password) {
+UserSchema.virtual('password').set(function (password) {
     this._password = password;
     this.salt = this.makeSalt();
     this.hashed_password = this.encryptPassword(password);
-}).get(function() {
+}).get(function () {
     return this._password;
 });
 
-/**
- * Validations
- */
-var validatePresenceOf = function(value) {
+var validatePresenceOf = function (value) {
     return value && value.length;
 };
-
-// the below 4 validations only apply if you are signing up traditionally
-UserSchema.path('name').validate(function(name) {
-    // if you are authenticating by any of the oauth strategies, don't validate
-    if (authTypes.indexOf(this.provider) !== -1) return true;
-    return name.length;
-}, 'Name cannot be blank');
-
-UserSchema.path('email').validate(function(email) {
-    // if you are authenticating by any of the oauth strategies, don't validate
-    if (authTypes.indexOf(this.provider) !== -1) return true;
-    return email.length;
-}, 'Email cannot be blank');
-
-UserSchema.path('username').validate(function(username) {
-    // if you are authenticating by any of the oauth strategies, don't validate
-    if (authTypes.indexOf(this.provider) !== -1) return true;
-    return username.length;
-}, 'Username cannot be blank');
-
-UserSchema.path('hashed_password').validate(function(hashed_password) {
-    // if you are authenticating by any of the oauth strategies, don't validate
-    if (authTypes.indexOf(this.provider) !== -1) return true;
-    return hashed_password.length;
-}, 'Password cannot be blank');
 
 
 /**
  * Pre-save hook
  */
-UserSchema.pre('save', function(next) {
+UserSchema.pre('save', function (next) {
     if (!this.isNew) return next();
 
     if (!validatePresenceOf(this.password) && authTypes.indexOf(this.provider) === -1)
@@ -94,7 +69,7 @@ UserSchema.methods = {
      * @return {Boolean}
      * @api public
      */
-    authenticate: function(plainText) {
+    authenticate: function (plainText) {
         return this.encryptPassword(plainText) === this.hashed_password;
     },
 
@@ -104,7 +79,7 @@ UserSchema.methods = {
      * @return {String}
      * @api public
      */
-    makeSalt: function() {
+    makeSalt: function () {
         return Math.round((new Date().valueOf() * Math.random())) + '';
     },
 
@@ -115,7 +90,7 @@ UserSchema.methods = {
      * @return {String}
      * @api public
      */
-    encryptPassword: function(password) {
+    encryptPassword: function (password) {
         if (!password) return '';
         return crypto.createHmac('sha1', this.salt).update(password).digest('hex');
     }
--- backend/services/users.js
+++ backend/services/users.js
@@ -27,73 +27,75 @@ var User = mongoose.model("User");
 var resumePathAsserted = false; // Boolean that keeps track of whether we have a resume folder or not
 // Some helper methods
 var randomString = function () {
-	// Return a string X characters long with random letters
-	var chars = [];
-	for (var i = 0; i < RANDOM_STR_LEN; i++)
-		chars.push(RANDOM_STR_CHARS.charAt(Math.floor(Math.random() * RANDOM_STR_CHARS.length)));
-	return chars.join(EMPTY_STRING);
+    // Return a string X characters long with random letters
+    var chars = [];
+    for (var i = 0; i < RANDOM_STR_LEN; i++)
+        chars.push(RANDOM_STR_CHARS.charAt(Math.floor(Math.random() * RANDOM_STR_CHARS.length)));
+    return chars.join(EMPTY_STRING);
 };
 
-/**
- * Show login form
- */
 var register = function (req, res) {
-	// Internally defined function to do user creation
-	var createUser = function (resumePath) {
-		// TODO do mongoose create based on req.body
-		console.log("createsypants");
-		console.log(req.body);
-	};
-	// Start piping for busboy
-	req.pipe(req.busboy);
-	// Busboy events for file
-	req.busboy.on("file", function (fieldName, file, fileName) {
-		var generatedFileName = randomString() + "." + fileName.split(".").pop();
-		logger.info("New resume being uploaded via member registration: \"" + fileName + "\" -> \"" + generatedFileName + "\"");
-		// Ensure that we have a place to put the file
-		fsUtil.assertPath(config.fs.resumePath, function (err) {
-			if (err) {
-				logger.error("Resume '" + generatedFileName + "' could not be uploaded - there was an exception.");
-				logger.error(err);
-				res.send(500, err);
-			} else {
-				var resumePath = path.join(config.fs.resumePath, generatedFileName);
-				file.pipe(fs.createWriteStream(resumePath));
-				logger.info("Upload of resume '" + generatedFileName + "' now completed.");
-				// Finish create
-				createUser(resumePath);
-			}
-		});
-	});
-	res.json(200, {});
+    var newUser = new User(req.body);
+    newUser.save(function (err) {
+        if (err) res.json(500, err);
+        else res.json(200, newUser);
+    });
+};
+
+var resumeDrop = function (req, res) {
+    // Start piping for busboy
+    req.pipe(req.busboy);
+    // Busboy events for file
+    req.busboy.on("file", function (fieldName, file, fileName) {
+        var generatedFileName = randomString() + "." + fileName.split(".").pop();
+        logger.info("New resume being uploaded via member registration: \"" + fileName + "\" -> \"" + generatedFileName + "\"");
+        // Ensure that we have a place to put the file
+        fsUtil.assertPath(config.fs.resumePath, function (err) {
+            if (err) {
+                logger.error("Resume '" + generatedFileName + "' could not be uploaded - there was an exception.");
+                logger.error(err);
+                res.send(500, err);
+            } else {
+                var resumePath = path.join(config.fs.resumePath, generatedFileName);
+                file.pipe(fs.createWriteStream(resumePath));
+                logger.info("Upload of resume '" + generatedFileName + "' now completed.");
+                // Finish create
+                res.send(200, generatedFileName);
+            }
+        });
+    });
 };
 
 var userNameFree = function (req, res) {
-	var username = req.param("username");
-	if (!username) {
-		res.send(400, "Username parameter required.");
-	} else {
-		User.find({
-			username: username
-		}).exec(function (err, results) {
-			if (err) {
-				res.json(500, err);
-			} else {
-				res.json(200, (!results || results.length === 0));
-			}
-		});
-	}
+    var username = req.param("username");
+    if (!username) {
+        res.send(400, "Username parameter required.");
+    } else {
+        User.find({
+            username: username
+        }).exec(function (err, results) {
+            if (err) {
+                res.json(500, err);
+            } else {
+                res.json(200, (!results || results.length === 0));
+            }
+        });
+    }
 };
 
 /******************************************* EXPORTS **********************************************/
 
 // This controller's HTTP routes
 module.exports.routes = [{
-	path: "/members/register",
-	method: "POST",
-	handler: register
+    path: "/members/register",
+    method: "POST",
+    handler: register
 }, {
-	path: "/members/isUserNameFree",
-	method: "GET",
-	handler: userNameFree
+    path: "/members/resumes/drop",
+    method: "POST",
+    handler: resumeDrop
+}, {
+    path: "/members/isUserNameFree",
+    method: "GET",
+    handler: userNameFree
 }];
