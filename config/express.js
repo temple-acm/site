376241056b0bb4767f2d717800b4fc4558b410fa
Sandile Keswa <me@sandile.io>
2014-07-21 02:33:48 -0400

    yikes. had to comment out existing 404 and 500 page code. we need to add that functionality back.

--- config/express.js
+++ config/express.js
@@ -63,21 +63,21 @@ module.exports = function (app, db) {
 		showStack: true
 	}));
 	//Assume "not found" in the error msgs is a 404. this is somewhat silly, but valid, you can do whatever you like, set properties, use instanceof etc.
-	app.use(function (err, req, res, next) {
+	// app.use(function (err, req, res, next) {
 		//Treat as 404
-		if (~err.message.indexOf('not found')) return next();
+	//	if (~err.message.indexOf('not found')) return next();
 		//Log it
-		console.error(err.stack);
+//		console.error(err.stack);
 		//Error page
-		res.status(500).render('500', {
-			error: err.stack
-		});
-	});
+//		res.status(500).render('500', {
+//			error: err.stack
+//		});
+//	});
 	//Assume 404 since no middleware responded
-	app.use(function (req, res, next) {
-		res.status(404).render('404', {
-			url: req.originalUrl,
-			error: 'Not found'
-		});
-	});
+//	app.use(function (req, res, next) {
+//		res.status(404).render('404', {
+//			url: req.originalUrl,
+//			error: 'Not found'
+//		});
+//	});
 };
