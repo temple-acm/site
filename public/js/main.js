1a4c1543c0b4d1c47b1e96a5a7d27f652a7a0a83
Sandile Keswa <me@sandile.io>
2014-08-06 00:05:14 -0400

    workin man

--- public/js/main.js
+++ public/js/main.js
@@ -90,12 +90,51 @@
 	//---- Services ----//
 
 	(function(module, app) {
+		module.controller('RegisterSvc', ['$http',
+			function($http) {
+				// This template is used to redirect to the payment form @ paypal
+				var PAYPAL_API_CALL_TEMPLATE = '<form action="https://www.paypal.com/cgi-bin/webscr" method="post"><input type="hidden" name="cmd"' +
+					' value="_s-xclick"><input type="hidden" name="hosted_button_id" value="6CYDNKB4YDGVC"><input type="hidden" name="notify_url" ' +
+					' value="http://acm.temple.edu/members/payments/callback/{{id}}"></form>';
+				// Does the same thing as PAYPAL_API_CALL_TEMPLATE - except it does it with fake money
+				var FAKE_PAYPAL_API_CALL_TEMPLATE = '<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post"><input type="hidden" name="cmd"' +
+					' value="_s-xclick"><input type="hidden" name="hosted_button_id" value="9M38CDYV8EHEJ"><input type="hidden" name="notify_url" ' +
+					' value="http://acm.temple.edu/members/payments/callback/{{id}}"></form>';
+				var PAYPAL_API_CALL_ID_TOKEN = '{{id}}';
 
+				this.isUserNameFree = function(userName) {
+					return $http({
+						method: 'GET',
+						url: '/members/isUserNameFree',
+						params: {
+							username: userName
+						}
+					});
+				};
+				this.registerUser = function(user) {
+					return $http({
+						method: 'POST',
+						url: '/members/register',
+						data: user
+					});
+				};
+				this.redirectToPaypal = function(userId) {
+					// TODO make this not the fake url on live
+					if (window.location.host === 'localhost' || window.location.host === '127.0.0.1') {
+						// We're doing testing
+						$(FAKE_PAYPAL_API_CALL_TEMPLATE.replace(PAYPAL_API_CALL_ID_TOKEN, userId)).submit();
+					} else {
+						$(PAYPAL_API_CALL_TEMPLATE.replace(PAYPAL_API_CALL_ID_TOKEN, userId)).submit();
+					}
+				};
+			}
+		]);
 	})(ng.module('services', []), _app);
 
 	//---- Controllers ----//
 
 	(function(module, app) {
+		// Main Controller
 		module.controller('MainCtrl', ['$scope',
 			function($scope) {
 				var ANIM_DELAY = 400;
@@ -138,6 +177,127 @@
 					$('overlay register').css('display', 'block');
 					$('overlay login').css('display', 'none');
 					showCard();
+				};
+			}
+		]);
+		// Registration Controller
+		module.controller('RegisterCtrl', ['$scope', '$rootScope', '$upload', 'RegisterSvc',
+			function($scope, $rootScope, $upload, registerService) {
+				// Constants
+				var SUBMIT_URL = "members/register";
+				var RESUME_DROP_URL = "members/resumes/drop";
+				var DRAG_DROP_NEUTRAL = "Drag & Drop Your Resume Here";
+				var DRAG_DROP_STYLE_NEUTRAL = "";
+				var DRAG_DROP_WRONG_TYPE = "Provided file was the wrong type";
+				var DRAG_DROP_WRONG_SIZE = "Provided file was too big";
+				var DRAG_DROP_STYLE_WRONG = "wrong";
+				var DRAG_DROP_RIGHT = "Resume file \"%s\" is ready to upload";
+				var DRAG_DROP_STYLE_RIGHT = "dropped";
+
+				// Instance variables
+				$scope.pendingFile = null;
+				$scope.dragDropMessage = DRAG_DROP_NEUTRAL;
+				$scope.dragDropStyle = DRAG_DROP_STYLE_NEUTRAL;
+
+				$scope.submit = function(user) {
+					if ($scope.registration.$valid) {
+						if ($scope.pendingFile) {
+							$scope.upload = $upload.upload({
+								url: RESUME_DROP_URL,
+								method: 'POST',
+								file: $scope.pendingFile
+							}).success(function() {
+								doRegistration(user);
+							}).error(function() {
+								alert('File upload failed.');
+								console.log('File upload failed', arguments);
+							});
+						} else {
+							registerService.registerUser(user).success(function(data, status, headers, config) {
+								$rootScope.me = data;
+								// Pass the id as a reference
+								console.log('result', data);
+								$scope.declareRegistered();
+								registerService.redirectToPaypal(data._id);
+							}).error(function(data, status, headers, config) {
+								$('#register-modal').modal("show");
+								console.log('problem', data);
+							});
+						}
+					} else {
+						// TODO modal or something
+						// $('#register-modal').modal("show");
+						alert('Could not submit registration form.');
+					}
+				};
+
+				$scope.selectFile = function($files, user) {
+					//$files: an array of files selected, each file has name, size, and type.
+					var file = $files[0]; // Assume just one file
+					var extension = file.name.split(".").pop().toLowerCase();
+					// Check extension
+					if (extension !== "doc" && extension !== "docx" && extension !== "pdf") {
+						$scope.dragDropStyle = DRAG_DROP_STYLE_WRONG;
+						$scope.dragDropMessage = DRAG_DROP_WRONG_TYPE;
+					} else if (file.size > 1048576) {
+						$scope.dragDropStyle = DRAG_DROP_STYLE_WRONG;
+						$scope.dragDropMessage = DRAG_DROP_WRONG_SIZE;
+					} else {
+						// Set the file instance variable
+						$scope.pendingFile = file;
+						// Cosmetics
+						$scope.dragDropStyle = DRAG_DROP_STYLE_RIGHT;
+						$scope.dragDropMessage = DRAG_DROP_RIGHT.replace("%s", file.name);
+					}
+				};
+
+				$scope.onUserIdChanged = function() {
+					var val = $("#userIdText").val();
+					if (val && val.length >= 5 && val.length <= 15 && /^[a-z0-9\.]+$/i.test(val)) {
+						RegisterService.isUserNameFree($("#userIdText").val(), function(err, isFree) {
+							if (isFree) {
+								$("#userIdIndicator").addClass("good");
+								$("#userIdIndicator").html("This user id is available.");
+								// Mark the field valid
+								$scope.registrationForm.userId.$setValidity("id", true);
+							} else {
+								$("#userIdIndicator").removeClass("good");
+								$("#userIdIndicator").html("This user id is taken.");
+								// Mark the field invalid
+								$scope.registrationForm.userId.$setValidity("id", false);
+							}
+						});
+					} else {
+						$("#userIdIndicator").removeClass("good");
+						$("#userIdIndicator").html("User id format invalid.");
+						// Mark the field invalid
+						$scope.registrationForm.userId.$setValidity("id", false);
+					}
+				};
+
+				$scope.onPasswordChanged = function() {
+					var pass = $("#passwordText").val();
+					var conf = $("#confirmPasswordText").val();
+					if (/^(?=.*[^a-zA-Z])(?=.*[a-z])(?=.*[A-Z])\S{8,}$/.test(pass)) {
+						RegisterService.isUserNameFree($("#userIdText").val(), function(err, isFree) {
+							if (pass === conf) {
+								$("#passwordIndicator").addClass("good");
+								$("#passwordIndicator").html("This password is valid.");
+								// Mark the field valid
+								$scope.registrationForm.password.$setValidity("pass", true);
+							} else {
+								$("#passwordIndicator").removeClass("good");
+								$("#passwordIndicator").html("The passwords don't match.");
+								// Mark the field invalid
+								$scope.registrationForm.password.$setValidity("pass", false);
+							}
+						});
+					} else {
+						$("#passwordIndicator").removeClass("good");
+						$("#passwordIndicator").html("Password format invalid.");
+						// Mark the field invalid
+						$scope.registrationForm.password.$setValidity("pass", false);
+					}
 				};
 			}
 		]);
--- services.js
+++ services.js
@@ -6,20 +6,25 @@ var LIVERELOAD_PLACEHOLDER = "<!-- Livereload -->";
 var INDEX_PAGE_PATH = path.join(__dirname, 'public', 'pages', 'index.html');
 
 exports.route = function(app) {
+	var pageCache = {};
 	// Main page route
 	app.get('/', function(req, res) {
 		if (process.env.TUACM_PRODUCTION)
 			res.sendfile(INDEX_PAGE_PATH);
 		else {
 			// Embed livereload
-			fs.readFile(INDEX_PAGE_PATH, function(err, data) {
-				if (err) {
-					res.send(500, err);
-				} else {
-					data = data.toString().replace(LIVERELOAD_PLACEHOLDER, LIVERELOAD_MIXIN);
-					res.send(data);
-				}
-			});
+			if (!pageCache.index) {
+				fs.readFile(INDEX_PAGE_PATH, function(err, data) {
+					if (err) {
+						res.send(500, err);
+					} else {
+						pageCache.index = data.toString().replace(LIVERELOAD_PLACEHOLDER, LIVERELOAD_MIXIN);
+						res.send(pageCache.index);
+					}
+				});
+			} else {
+				res.send(pageCache.index);
+			}
 		}
 	});
 	// Get the slides
