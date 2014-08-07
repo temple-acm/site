6e80dc0466722db41d54391e1eac825537f05952
Sandile Keswa <me@sandile.io>
2014-08-07 15:32:48 -0400

    now supports url mistakes

--- services.js
+++ services.js
@@ -29,4 +29,26 @@ exports.route = function(app) {
 			res.json(results);
 		});
 	});
+	// Redirects due to user error
+	app.get('/register', function(req, res) {
+		res.redirect('/#/register');
+	});
+	app.get('/login', function(req, res) {
+		res.redirect('/#/login');
+	});
+	app.get('/emailus', function(req, res) {
+		res.redirect('/#/emailus');
+	});
+	app.get('/about', function(req, res) {
+		res.redirect('/#/about');
+	});
+	app.get('/events', function(req, res) {
+		res.redirect('/#/events');
+	});
+	app.get('/contact', function(req, res) {
+		res.redirect('/#/contact');
+	});
+	app.get('/dev', function(req, res) {
+		res.redirect('/#/dev');
+	});
 };
