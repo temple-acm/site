26131b675fdfd406d8a6b7291bc255e85e91c1b2
Sandile Keswa <me@sandile.io>
2014-08-25 16:15:31 -0400

    Got rid of the hold over js from the template

--- public/js/main.js
+++ public/js/main.js
@@ -467,6 +467,38 @@
 				// Code that handles the "Email Us" form goes here
 			}
 		]);
+		// Google Map Controller
+		module.controller('MapCtrl', ['$scope',
+			function() {
+				// Google Maps Scripts
+				// When the window has finished loading create our google map below
+				google.maps.event.addDomListener(window, 'load', function() {
+					// Basic options for a simple Google Map
+					// For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+					var mapOptions = {
+						// How zoomed in you want the map to start at (always required)
+						zoom: 16,
+						// The latitude and longitude to center the map (always required)
+						center: new google.maps.LatLng(39.981489, -75.153174), // New York
+						// Disables the default Google Maps UI components
+						disableDefaultUI: false,
+						scrollwheel: false,
+						draggable: true
+					};
+					// Get the HTML DOM element that will contain your map
+					// We are using a div with id="map" seen below in the <body>
+					var mapElement = document.getElementById('map');
+					// Create the Google Map using out element and options defined above
+					var map = new google.maps.Map(mapElement, mapOptions);
+					// Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
+					var myLatLng = new google.maps.LatLng(39.981489, -75.153174);
+					var beachMarker = new google.maps.Marker({
+						position: myLatLng,
+						map: map
+					});
+				});
+			}
+		]);
 		// Login Controller
 		module.controller('LoginCtrl', ['$scope', '$rootScope', 'LoginSvc',
 			function($scope, $rootScope, service) {
