869c1f924bdb7ff2a9b9b29ec439f6a1f7e94299
Sandile Keswa <me@sandile.io>
2014-08-31 04:34:41 -0400

    Added paypal IPN callback support for marking people as paid. This needs to be verified in production to make sure we are receiving requests properly.

--- routes/payment.js
+++ routes/payment.js
@@ -1,21 +1,31 @@
+var ipn = require('paypal-ipn');
+var logger = require('../util/log');
+
 exports.route = function(app) {
 	// Callback we get from successful payment from Paypal.
 	// Updates the "paid" field for that user in the database.
-	var paypalCallback = function(req, res) {
+	app.post('/members/payments/callback/:userName', function(req, res) {
+		// IPN requires that we send a 200 immediately
+		res.send(200);
 		var userName = req.param('userName');
-		req.db.collection('users').update({
-			userName: userName
-		}, {
-			paid: true,
-			dateLastPaid: (new Date()).getTime()
-		}, {}, function(err) {
+		ipn.verify(req.body, function(err, msg) {
 			if (err) {
-				logger.log('error', 'could not mark user paid', err);
+				logger.log('error', 'could not process IPN callback for ' + userName, err);
 			} else {
-				logger.log('debug', 'User "', userName, '" is marked paid.');
+				// We received a bonafide response
+				req.db.collection('users').update({
+					userName: userName
+				}, {
+					paid: true,
+					dateLastPaid: (new Date()).getTime()
+				}, {}, function(err) {
+					if (err) {
+						logger.log('error', 'could not mark user paid', err);
+					} else {
+						logger.log('debug', 'User "', userName, '" is marked paid.');
+					}
+				});
 			}
 		});
-	};
-	app.get('/members/payments/callback/:userName', paypalCallback);
-	app.post('/members/payments/callback/:userName', paypalCallback);
+	});
 };
