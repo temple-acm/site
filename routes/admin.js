b125bd2781f2ff55a75ce9ec7a808e5a138c0a63
Sri Ramanujam <sri.ramanujam@temple.edu>
2014-10-17 18:52:19 -0400

    Partial progress on backend for admin stuffs

--- routes/admin.js
+++ routes/admin.js
@@ -4,9 +4,144 @@
 
 var passport = require('passport'),
     logger = require('../util/log'),
-    acl = require('acl');
+    acl = require('acl'),
+    ObjectId = require('mongodb').ObjectID;
 
 exports.route = function(app) {
+
+    /*
+     * This endpoint fetches a single slide's data from the database.
+     *
+     *  Input:
+     *      order: the ordering number of the slide to be fetched.
+     *  Output:
+     *      Success:
+     *          status: 200
+     *          data: JSON document representing the slide
+     *      Failure:
+     *          Slide num not present for call:
+     *              status: 200
+     *              data: { '500' : 'Unspecified error' }
+     *          DB error:
+     *              status: 200
+     *              data: { '500' : 'Unspecified error' }
+     *          Unauthorized access:
+     *              status: 403
+     */
+    app.get('/admin/getSlide', acl.middleware(1), function(req, res) {
+        if (req.body.slideOrderNum) {
+            req.db.findOne({
+                order: req.body.slideOrderNum
+            }, {
+                limit: 1
+            }, function(err, slideDoc) {
+                if (err) {
+                    logger.log('error', 'DB query gone wrong in getSlide', err);
+                    res.status(200).send({
+                        '500' : 'Unspecified Error'
+                    });
+                } else {
+                    res.status(200).json(slideDoc);
+                }
+            });
+        } else {
+            //how do you mess this up it's literally one number
+            logger.log('error', 'Slide number missing in call to getSlide', err);
+            res.status(200).send({
+                '500' : 'Unspecified error'
+            });
+        }
+    });
+
+    /*
+     * This endpoint adds a banner slide to the database. Be sure to properly
+     * pass in all three required parts.
+     *
+     * TODO: Doesn't work yet, see inline todo comments.
+     *
+     *  Input:
+     *      slideHTML: The HTML to go along with the slide. Optional.
+     *      slideLink: The link to the banner image (because screw self-hosting amirite)
+     *      slideTitle: The title of the slide
+     *      slideSubtitle: The subtitle of the slide
+     *  Output:
+     *      Success:
+     *          status: 200
+     *          data: { '200' : 'OK' }
+     *      Failure:
+     *          Unauthorized access:
+     *              status: 403
+     *          DB error:
+     *              status: 200
+     *              data: { '500' : 'Unspecified error' }
+     *          Invalid input:
+     *              status: 200
+     *              data: { '500' : 'Unspecified error' }
+     */
+    app.post('/admin/addSlide', acl.middleware(1), function(req, res) {
+        if (req.body.slideLink && req.body.slideTitle && req.body.slideSubtitle) {
+            req.db.collection('slides').save({
+                //TODO: Do we need to change the schema? How will this work
+                //with people actuall trying to access this DB?
+            }, {}, function(err, createdSlide) {});
+        } else {
+            // wow these muppets forgot an element. what a bunch of noobcakes
+            // TODO: maybe we say exactly which element is missing, that'd probably help debugging
+            logger.log('error', 'Required slide element missing', err);
+            res.status(200).send({
+                '500' : 'Unspecified error'
+            });
+        }
+    });
+
+    /*
+     * This endpoint removes a banner slide from the database.
+     * It takes as its single argument an ObjectID corresponding to the slide
+     * entry that is to be deleted.
+     *
+     *  Output:
+     *      slideObjectID: The ObjectID of the slide to be deleted.
+     *  Output:
+     *      Success:
+     *          status: 200
+     *          data: { '200' : 'OK' }
+     *      Failure:
+     *          ObjectID not present:
+     *              status: 200
+     *              data: {'500' : 'Unspecified error' }
+     *          DB error:
+     *              status: 200
+     *              data: { '500' : 'Unspecified error' }
+     *      Unauthorized access:
+     *          status: 403
+     */
+    app.post('/admin/removeSlide', acl.middleware(1), function(req, res) {
+        if (req.body.slideObjectID) {
+            removeObjectId = new ObjectId(req.body.slideObjectID);
+            req.db.collection('slides').remove({
+                _id: removeObjectId
+            }, {
+                w: 1,
+            }, function(err, numRemovedDocs) {
+                if (err || numRemovedDocs != 1) {
+                    logger.log('error', 'Something went wrong removing the entry from the db', err);
+                    res.status(200).send({
+                        '500' : 'Unspecified error'
+                    });
+                } else {
+                    res.status(200).send({
+                        '200' : 'OK'
+                    });
+                }
+            });
+        } else {
+            // wow how do you mess this up
+            logger.log('error', 'slide ObjectID not present', err);
+            res.status(200).send({
+                '500' : 'Unspecified error
+            });
+        }
+    });
 
     /*
      * This endpoint removes the officer status of the account specified.
@@ -71,7 +206,7 @@ exports.route = function(app) {
      * This endpoint updates an account's status to "officer" in the backend db,
      * given the relevant officer-only information.
      *
-     * TODO: Access control?
+     * TODO: Access control? First pass at implementation using acl module
      *
      *  Input:
      *      accountName: the username of the account to elevate
@@ -95,12 +230,12 @@ exports.route = function(app) {
         if (req.body.accountName && req.body.accountTitle) {
             if (req.body.accountBio) {
                 req.db.collection('users').update({
-                    userName: accountName
+                    userName: req.body.accountName
                 }, {
                     $set: {
-                        title: accountTitle,
+                        title: req.body.accountTitle,
                         officer: true,
-                        bio: accountBio
+                        bio: req.body.accountBio
                     }
                 }, {
                     multi: false,
@@ -126,7 +261,8 @@ exports.route = function(app) {
                         officer: true
                     }
                 }, {
-                    multi: false
+                    multi: false,
+                    upsert: true // TODO: Check if this is necessary
                 }, function(err) {
                     if (err) {
                         logger.log('error', 'could not mark user ' + req.body.accountName + ' as officer', err);
